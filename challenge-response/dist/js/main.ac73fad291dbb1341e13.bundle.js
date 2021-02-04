4a4184269e45d77f15687c98ba25ffd386cb87f5
Mina <gogo.mina@gmail.com>
2021-02-04 20:55:09 +1030

    Include the complied version - dist folder

---
+++ challenge-response/dist/js/main.ac73fad291dbb1341e13.bundle.js
@@ -0,0 +1,1 @@
+(self.webpackChunkwebpack_boilerplate=self.webpackChunkwebpack_boilerplate||[]).push([[179],{579:()=>{"use strict";var e=window.jQuery;e((function(){e(".slider").slick({arrows:!1,fade:!0,dots:!0})}))}},0,[[579,666]]]);
